f97badac63984430167a0c72392780c228d97cd4
alessandro ricci <phd.alessandro.ricci@gmail.com>
2015-06-30 23:40:01 +0200

    bkt update: different trading time scale avaliable

--- backtest real/bktOffline.m
+++ backtest real/bktOffline.m
@@ -1,86 +1,119 @@
-function [outputBktOffline]=bktOffline(nData,histName)
-% Commenti al codice sono contrassegnati da by_ivan
-% In generale e' molto ben fatto e non vedo errori particolari.
-% Per ora non ho matlab e' non posso fare il debug, aspetto Simone che crei la macchina virtuale,
-% ma se gia' facessi il push del dummy storico e del'algo di test, potrei darti un'idea piu' precisa.
-% Se lo hai gia' fatto, non sono riuscito a trovare i file.
-%
-% DESCRIPTION:
-% -------------------------------------------------------------
-% This function runs the offline backtest on given historical data
-%
-%
-% INPUT parameters:
-% -------------------------------------------------------------
-% nData             ...
-% histName          ... 'storico.csv'
-%                   ...
-%                   ...
-%
-% OUTPUT parameters:
-% -------------------------------------------------------------
-%
-%
-%
-% EXAMPLE of use:
-% -------------------------------------------------------------
-%
-%
-
-
-storico = csvread(histName);
-startingOperation=0;
-indexResult=0;
-
-ls=length(storico);
-
-for i=nData:ls
+classdef bktOffline < handle
     
-    [oper, openValue, closeValue, stopLoss, noLoose, valueTp] = Algo_testBktOffline(storico(i-(nData-1):i,:));
-% newState=Algo_testBktOffline(storico(i-(nData-1):i,:));
-      
-        newState{1}=oper;
-        newState{2}=openValue;
-        newState{3}=closeValue;
-        newState{4}=stopLoss;
-        newState{5}=noLoose;
-        newState{6}=valueTp;
-
-        updatedOperation=newState{1};
-        
-        if abs(updatedOperation)>0 && startingOperation==0
-            indexResult=indexResult+1;
-            startingOperation=newState{1};
+    properties
+        newHisData
+        outputBktOffline
+    end
+    
+    methods
+ 
+        function [obj]=spin(obj,nData,histName,newTimeScale)
+            % Commenti al codice sono contrassegnati da by_ivan
+            % In generale e' molto ben fatto e non vedo errori particolari.
+            % Per ora non ho matlab e' non posso fare il debug, aspetto Simone che crei la macchina virtuale,
+            % ma se gia' facessi il push del dummy storico e del'algo di test, potrei darti un'idea piu' precisa.
+            % Se lo hai gia' fatto, non sono riuscito a trovare i file.
+            %
+            % DESCRIPTION:
+            % -------------------------------------------------------------
+            % This function runs the offline backtest on given historical data
+            %
+            %
+            % INPUT parameters:
+            % -------------------------------------------------------------
+            % nData             ...
+            % histName          ... 'storico.csv'
+            %                   ...
+            %                   ...
+            %
+            % OUTPUT parameters:
+            % -------------------------------------------------------------
+            %
+            %
+            %
+            % EXAMPLE of use:
+            % -------------------------------------------------------------
+            % bkt_AlgoX=bktOffline
+            % bkt_AlgoX=bkt_AlgoX.spin(10,'dummy_hist.csv',2)
+            %
             
-            display(indexResult);
-            display(startingOperation);
-            
-            direction(indexResult)=newState{1};
-            openingPrice(indexResult)=newState{2};
-            
-        elseif updatedOperation==0 && abs(startingOperation)>0
-            closingPrice(indexResult)=newState{3};   
-            display(closeValue);
+            hisData = csvread(histName);
             startingOperation=0;
-            display('operation closed');
+            indexOpen=0;
+            indexClose=0;
+            
+            if newTimeScale>1
+                expert=TimeSeriesExpert_11;
+                expert.rescaleData(hisData,1,newTimeScale)
+                
+                obj.newHisData(:,1)=expert.openVrescaled;
+                obj.newHisData(:,2)=expert.maxVrescaled;
+                obj.newHisData(:,3)=expert.minVrescaled;
+                obj.newHisData(:,4)=expert.closeVrescaled;
+                obj.newHisData(:,5)=expert.volrescaled;
+                obj.newHisData(:,6)=expert.openDrescaled;
+                
+            else
+                obj.newHisData=hisData;
+            end
+            
+            ls=length(obj.newHisData);
+            direction=zeros(floor(ls/2));
+            openingPrice=zeros(floor(ls/2));
+            closingPrice=zeros(floor(ls/2));
+            
+            for i=nData:ls
+                
+                [oper, openValue, closeValue, stopLoss, noLoose, valueTp] = Algo_testBktOffline(obj.newHisData(i-(nData-1):i,:));
+                
+                newState{1}=oper;
+                newState{2}=openValue;
+                newState{3}=closeValue;
+                newState{4}=stopLoss;
+                newState{5}=noLoose;
+                newState{6}=valueTp;
+                
+                updatedOperation=newState{1};
+                
+                if abs(updatedOperation)>0 && startingOperation==0
+                    indexOpen=indexOpen+1;
+                    startingOperation=newState{1};
+                    
+                    display(indexOpen);
+                    display(startingOperation);
+                    
+                    direction(indexOpen)=newState{1};
+                    openingPrice(indexOpen)=newState{2};
+                    
+                elseif updatedOperation==0 && abs(startingOperation)>0
+                    indexClose=indexClose+1;
+                    closingPrice(indexOpen)=newState{3};
+                    display(closeValue);
+                    startingOperation=0;
+                    display('operation closed');
+                end
+                
+            end
+            
+            direction=direction(1:indexClose);
+            openingPrice=openingPrice(1:indexClose);
+            l=length(direction);
+            
+            obj.outputBktOffline=zeros(l,8);
+            
+            % outputBktOffline(:,1)=nCandelotto;           % index of stick
+            obj.outputBktOffline(:,2)=openingPrice(1:l);          % opening price
+            obj.outputBktOffline(:,3)=closingPrice(1:l);          % closing price
+            % outputBktOffline(:,4)=matrixWeb(:,4);        % returns
+            obj.outputBktOffline(:,5)=direction(1:l);             % direction
+            obj.outputBktOffline(:,6)=ones(l,1);              % real
+            % outputBktOffline(:,7)=openingDateNum;        % opening date in day to convert use: d2=datestr(outputDemo(:,2), 'mm/dd/yyyy HH:MM')
+            % outputBktOffline(:,8)=closingDateNum;        % closing date in day to convert use: d2=datestr(outputDemo(:,2), 'mm/dd/yyyy HH:MM')
+            
         end
-
+        
+    end
+    
 end
-
-if length(direction)>length(closingPrice)
-    l=length(direction)-1;
-else
-    l=length(direction);
-end
-outputBktOffline=zeros(l,8);
-
-% outputBktOffline(:,1)=nCandelotto;           % index of stick
-outputBktOffline(:,2)=openingPrice(1:l);          % opening price
-outputBktOffline(:,3)=closingPrice(1:l);          % closing price
-% outputBktOffline(:,4)=matrixWeb(:,4);        % returns
-outputBktOffline(:,5)=direction(1:l);             % direction
-outputBktOffline(:,6)=ones(l,1);              % real
-% outputBktOffline(:,7)=openingDateNum;        % opening date in day to convert use: d2=datestr(outputDemo(:,2), 'mm/dd/yyyy HH:MM')
-% outputBktOffline(:,8)=closingDateNum;        % closing date in day to convert use: d2=datestr(outputDemo(:,2), 'mm/dd/yyyy HH:MM')
-
-
+    
+    
