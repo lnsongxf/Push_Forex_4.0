cfc605b0a67ea51313757e75face5e120782c6a0
simone.allemanini@gmail.com <simone.allemanini@gmail.com>
2015-07-12 18:01:44 +0100

    node - Mt4 intagration

---
+++ nodeM/node_metatrader.js
@@ -0,0 +1,30 @@
+var http = require('http');
+// Create a socket 
+var zmq = require('zmq');
+var sock = zmq.socket('sub');
+ 
+
+// pubber.js 
+
+// producer.js
+//sock.bindSync('tcp://127.0.0.1:50005');
+
+
+// subber.js 
+
+sock.connect('tcp://127.0.0.1:50004');
+sock.subscribe('cmd1');
+console.log('Subscriber connected to port 50004');
+
+sock.on('message', function(topic, message) {
+  console.log('received a message related to:', topic, 'containing message:', message);
+});
+
+
+
+/*sock.connect('tcp://127.0.0.1:50004');
+console.log('Worker connected to port 3000');
+
+sock.on('message', function(msg){
+  console.log('work: %s', msg.toString());
+});*/
