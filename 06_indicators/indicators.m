31809db31105b095cfc919ffa80b9e2b210bc353
alessandro ricci <phd.alessandro.ricci@gmail.com>
2015-08-10 21:06:47 +0200

    new class indicators has been created

---
+++ 06_indicators/indicators.m
@@ -0,0 +1,21 @@
+classdef indicators < handle
+    
+            %
+            % DESCRIPTION:
+            % -------------------------------------------------------------
+            % This class collect many functions for the calculation of
+            % specific trading indicators useful for bulding up the coreState of
+            % the Algos. Ideed all the indicators useful for bulding up an entry/exit
+            % strategy have to be included as method of this class.
+            %
+
+    properties
+
+    end
+    
+    
+    methods
+        
+    end
+    
+end
