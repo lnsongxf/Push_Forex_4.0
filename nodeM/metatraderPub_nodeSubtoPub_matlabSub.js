932eb128a54cf8d4057c5f817f658267afb38e58
unknown <simone.allemanini@gmail.com>
2015-08-24 17:28:55 +0100

    broker node , added feature for scheduling time

--- nodeM/metatraderPub_nodeSubtoPub_matlabSub.js
+++ nodeM/metatraderPub_nodeSubtoPub_matlabSub.js
@@ -1,12 +1,13 @@
 var http = require('http');
 var zmq = require('zmq'); 
+var schedule = require('node-schedule');
 
 var QuotesModule = (function(){
 
 	var _timeFrameQuotes = function(providerName){
 		this.provider = providerName, 
 		this.description = "This obj store all the time-frame quotes from this specific Provider and for each cross"
-	}
+	};
 
 	var _createTimeFrameQuotesObj = function(quotes_list,providerName){
 
@@ -30,7 +31,7 @@ var QuotesModule = (function(){
 	var _realTimeQuotes = function(providerName){
 		this.provider = providerName, 
 		this.description = "This obj store all the last current quotes from this specific provider and for each cross"
-	}
+	};
 
 	var _createRealTimeQuotesObj = function(quotes_list,providerName){
 		var _realTimeQuotesObj = new _realTimeQuotes(providerName);
@@ -44,6 +45,21 @@ var QuotesModule = (function(){
 		    }
 		}
 		return _realTimeQuotesObj
+	};
+
+	var _updateRealTimeQuotesObj = function(searchObjRealTimeQuote,messageArr){
+		for (var key0 in runningProviderRealTimeObjs) {
+			if (key0 == searchObjRealTimeQuote) {
+  				for (var key in runningProviderRealTimeObjs[key0]) {
+			  		if (runningProviderRealTimeObjs[key0].hasOwnProperty(key)) {
+			  			if (key == messageArr[0]) {
+			  				runningProviderRealTimeObjs[key0][key] = messageArr[1];
+			  				return true;
+			  			};	
+			  		}
+				}
+			}
+		}
 	};
 
 	var _updateTimeFrameQuotesObj = function(timeFrame,timeFrameQuotesObj,realTimeQuotesObj){
@@ -114,6 +130,44 @@ var QuotesModule = (function(){
 		return timeFrameQuotesObj;
 	};
 
+	var _importHistoryTimeFrameQuotesObj = function(searchObjRealTimeQuote,messageArr){
+		for (var key0 in runningProviderTimeFrameObjs) {
+			if (key0 == searchObjTimeFrameQuote) {
+				for (var key in runningProviderTimeFrameObjs[key0]) {
+					if (runningProviderTimeFrameObjs[key0].hasOwnProperty(key)) {
+			  			if (key == messageArr[0]) {
+			  				for(var i=0;i<runningProviderTimeFrameObjs[key0][key].length;i++){
+			  					for (var key1 in runningProviderTimeFrameObjs[key0][key][i]) {
+			  						var tmpObjTimeFrameQuote = runningProviderTimeFrameObjs[key0][key][i];
+			  						if ( Object.keys(tmpObjTimeFrameQuote)[0] == messageArr[1] ) {
+			  							var arrfirstQuotesValues = messageArr[2].split("$");
+			  							for(var k=0;k<arrfirstQuotesValues.length;k++){
+			  								for(var j=0;j<tmpObjTimeFrameQuote[Object.keys(tmpObjTimeFrameQuote)[0]].length;j++){
+			  									var tmpObjSetValuesQuote = tmpObjTimeFrameQuote[Object.keys(tmpObjTimeFrameQuote)[0]][j];
+			  									if (tmpObjSetValuesQuote[Object.keys(tmpObjSetValuesQuote)].length < Object.keys(tmpObjSetValuesQuote)[0].split("v")[1] ){
+			  										tmpObjSetValuesQuote[Object.keys(tmpObjSetValuesQuote)].push(arrfirstQuotesValues[k]);
+			  										tmpObjTimeFrameQuote[Object.keys(tmpObjTimeFrameQuote)[0]][j] = tmpObjSetValuesQuote;
+			  										runningProviderTimeFrameObjs[key0][key][i] = tmpObjTimeFrameQuote;
+			  										return true;
+			  									}else{
+			  										tmpObjSetValuesQuote[Object.keys(tmpObjSetValuesQuote)].shift();
+													tmpObjSetValuesQuote[Object.keys(tmpObjSetValuesQuote)].push(arrfirstQuotesValues[k]);
+													tmpObjTimeFrameQuote[Object.keys(tmpObjTimeFrameQuote)[0]][j] = tmpObjSetValuesQuote;
+			  										runningProviderTimeFrameObjs[key0][key][i] = tmpObjTimeFrameQuote;
+			  										return true;
+			  									}
+			  								}
+			  							}	
+			  						}			
+			  					}
+			  				}
+						}
+					}
+				}
+			}
+		}
+	};
+
 	return{
     	createTimeFrameQuotesObj: function(quotes_list,providerName){ 
       		return _createTimeFrameQuotesObj(quotes_list,providerName);  
@@ -123,6 +177,12 @@ var QuotesModule = (function(){
     	},
     	updateTimeFrameQuotesObj: function(timeFrame,timeFrameQuotesObj,realTimeQuotesObj){
     		return _updateTimeFrameQuotesObj(timeFrame,timeFrameQuotesObj,realTimeQuotesObj);
+    	},
+    	updateRealTimeQuotesObj: function(searchObjRealTimeQuote,messageArr){
+    		return _updateRealTimeQuotesObj(searchObjRealTimeQuote,messageArr);
+    	},
+    	importHistoryTimeFrameQuotesObj: function(searchObjTimeFrameQuote,messageArr){
+    		return _importHistoryTimeFrameQuotesObj(searchObjTimeFrameQuote,messageArr);
     	}
     }
 
@@ -137,37 +197,49 @@ sockSubFromSignalProvider.bindSync('tcp://127.0.0.1:50026');
 sockPub.bindSync('tcp://127.0.0.1:50027');  
 
 //-------------------------------------------------------------------------------------------------------------------------------
-// METATRADER PUB TO NODEJS TO MATLAB
+// QUOTES PROVIDER PUB TO NODEJS TO SIGNAL PROVIDER
 
-var configQuotesList = require('./mt4_config_quotes');
+var configQuotesList = require('./config_quotes');
+//REMEMBER THAT THE MONTH IN THE SERVER SETTING JSON START FROM 0 TO 11
+//Month: Integer value representing the month, beginning with 0 for January to 11 for December
+var serverSetting = require('./server_setting');
 var runningProviderTopicList = [];
 var runningProviderTimeFrameObjs = {};
 var runningProviderRealTimeObjs = {};
 
-setInterval(function() {    
 
-	if (runningProviderTopicList.length > 0) {
-		for (var i = 0; i < runningProviderTopicList.length; i++) {
-	    	var tmpTopicArr = runningProviderTopicList[i].toString().split("@");
-	    	//TOPIC EXAMPLE: "MT4@ACTIVTRADES@REALTIMEQUOTES";
-	    	var tmpTimeFrameQuoteProperty = "TIMEFRAMEQUOTE$"+tmpTopicArr[0]+"$"+tmpTopicArr[1];
-	    	var tmpRealTimeQuoteProperty = "REALTIMEQUOTE$"+tmpTopicArr[0]+"$"+tmpTopicArr[1];
-	    	//EX Time Frame Obj Property: runningProviderTimeFrameObjs["TIMEFRAMEQUOTE$MT4$ACTIVTRADES"];
-	   		var new_timeFrameQuotesObj = QuotesModule.updateTimeFrameQuotesObj("m1",runningProviderTimeFrameObjs[tmpTimeFrameQuoteProperty],runningProviderRealTimeObjs[tmpRealTimeQuoteProperty]);
-	   		runningProviderTimeFrameObjs[tmpTimeFrameQuoteProperty] = new_timeFrameQuotesObj;
-		}
-	}
+// THE CODE BELOW SET THE SUBTASK TO UPDATED THE TIMEFRAME DATA EACH 1M,5M,15M ETC.. 
+var startSchedule = serverSetting.serverSettingList[0].startScheduleTime.split(",");
+var date_start_schedule = new Date(startSchedule[0],startSchedule[1],startSchedule[2],startSchedule[3],startSchedule[4],startSchedule[5]);
+var minutesList=[{'m1':60000},{'m5':300000},{'m15':900000},{'m30':1800000},{'h1':3600000},{'h4':14400000},{'d1':86400000},{'w1':604800000}];
+var startTask0 = schedule.scheduleJob(date_start_schedule, function(){
+    //console.log('Start Scheduling! Scheduled Time:'+startSchedule+'   Current Time: '+Date());
+    for (var i = minutesList.length - 1; i >= 0; i--) {
+    	console.log("subtasks: ",minutesList[i][Object.keys(minutesList[i])[0]]," ",Object.keys(minutesList[i])[0]);
+    
+    	setInterval(function() {  
+	    	console.log('Start Scheduling 1M! Current Time: '+Date());  
 
-},5000);  // 1M
-setInterval(function() {         },300000);  // 5M
-setInterval(function() {         },900000);  // 15M
-setInterval(function() {         },1800000);  // 30M
-setInterval(function() {         },3600000);  // 60M - 1H
-setInterval(function() {         },14400000);  // 240M - 4H
-setInterval(function() {         },86400000);  // 1440M - 24H
-setInterval(function() {         },604800000);  // 10080M - 1W
+			if (runningProviderTopicList.length > 0) {
+				for (var i = 0; i < runningProviderTopicList.length; i++) {
+			    	var tmpTopicArr = runningProviderTopicList[i].toString().split("@");
+			    	//TOPIC EXAMPLE: "MT4@ACTIVTRADES@REALTIMEQUOTES";
+			    	var tmpTimeFrameQuoteProperty = "TIMEFRAMEQUOTE$"+tmpTopicArr[0]+"$"+tmpTopicArr[1];
+			    	var tmpRealTimeQuoteProperty = "REALTIMEQUOTE$"+tmpTopicArr[0]+"$"+tmpTopicArr[1];
+			    	//EX Time Frame Obj Property: runningProviderTimeFrameObjs["TIMEFRAMEQUOTE$MT4$ACTIVTRADES"];
+			   		var new_timeFrameQuotesObj = QuotesModule.updateTimeFrameQuotesObj(Object.keys(minutesList[i])[0],runningProviderTimeFrameObjs[tmpTimeFrameQuoteProperty],runningProviderRealTimeObjs[tmpRealTimeQuoteProperty]);
+			   		runningProviderTimeFrameObjs[tmpTimeFrameQuoteProperty] = new_timeFrameQuotesObj;
+				}
+			}
+		},minutesList[i][Object.keys(minutesList[i])[0]]);  // 1M
+    };
+});
+
+
 
 sockSubFromQuotesProvider.subscribe('NEWTOPICQUOTES');
+sockSubFromQuotesProvider.subscribe('DELETETOPICQUOTES');
+sockSubFromQuotesProvider.subscribe('');
 sockSubFromQuotesProvider.on('message', function(topic, message) {
 	var topicArr = topic.toString().split("@");
   	var messageArr = message.toString().split("@");
@@ -176,8 +248,12 @@ sockSubFromQuotesProvider.on('message', function(topic, message) {
   		case "NEWTOPICQUOTES":
   			//TOPIC MESSAGE EXAMPLE: "MT4@ACTIVTRADES@REALTIMEQUOTES";
   			if ( runningProviderTopicList.indexOf( message.toString() ) == "-1" ) {
+  				var operationFeedbackStatusTopic = messageArr[0]+"@"+messageArr[1]+"@OPERATIONSTATUS";
+				//CREATE AND ADD NEW TOPICS (EX: MT4@ACTIVTRADES@REALTIMEQUOTES, MT4@ACTIVTRADES@OPERATIONSTATUS) IN THE ARRAY LIST
 				runningProviderTopicList.push(message.toString());
+				runningProviderTopicList.push(operationFeedbackStatusTopic); 
 				sockSubFromQuotesProvider.subscribe(message.toString());
+				sockSubFromQuotesProvider.subscribe(operationFeedbackStatusTopic);
 
 				if ( messageArr[2] == "REALTIMEQUOTES" ||  messageArr[2] == "LISTQUOTES"){
 					var newObjTimeFrameQuote = "TIMEFRAMEQUOTE$"+messageArr[0]+"$"+messageArr[1];
@@ -193,9 +269,16 @@ sockSubFromQuotesProvider.on('message', function(topic, message) {
 		case "DELETETOPICQUOTES":
 			//TOPIC EXAMPLE: "MT4@ACTIVTRADES@REALTIMEQUOTES";
 			if ( runningProviderTopicList.indexOf( message.toString() ) > -1 ){
+				//REMOVE TOPICS (EX: MT4@ACTIVTRADES@REALTIMEQUOTES) IN THE ARRAY LIST
 				var index = runningProviderTopicList.indexOf( message.toString() );
 				runningProviderTopicList.splice(index, 1);
 				sockSubFromQuotesProvider.unsubscribe(message.toString());
+
+				//REMOVE TOPICS (EX: MT4@ACTIVTRADES@OPERATIONSTATUS) IN THE ARRAY LIST
+				var operationFeedbackStatusTopic = messageArr[0]+"@"+messageArr[1]+"@OPERATIONSTATUS";
+				var index_operation_status_topic = runningProviderTopicList.indexOf( operationFeedbackStatusTopic );
+				runningProviderTopicList.splice(index_operation_status_topic, 1);
+				sockSubFromQuotesProvider.unsubscribe( operationFeedbackStatusTopic );
 
   				var searchObjTimeFrameQuote = "TIMEFRAMEQUOTE$"+messageArr[0]+"$"+messageArr[1];
 				var searchObjRealTimeQuote = "REALTIMEQUOTE$"+messageArr[0]+"$"+messageArr[1];
@@ -211,52 +294,16 @@ sockSubFromQuotesProvider.on('message', function(topic, message) {
 				var searchObjTimeFrameQuote = "TIMEFRAMEQUOTE$"+topicArr[0]+"$"+topicArr[1];
 
 				if (messageArr.length == 2) {
-					for (var key0 in runningProviderRealTimeObjs) {
-						if (key0 == searchObjRealTimeQuote) {
-			  				for (var key in runningProviderRealTimeObjs[key0]) {
-						  		if (runningProviderRealTimeObjs[key0].hasOwnProperty(key)) {
-						  			if (key == messageArr[0]) {
-						  				runningProviderRealTimeObjs[key0][key] = messageArr[1];
-						  			};	
-						  		}
-							}
-						}
-					}
+					if (messageArr[2] == "REALTIMEQUOTES" ) {
+						var result = QuotesModule.updateRealTimeQuotesObj(searchObjRealTimeQuote,messageArr);
+						if (result) {}else{console.log("error update real time Obj")};
+					}else if (messageArr[2] == "OPERATIONSTATUS") {
+						//SEND OPERATION MESSAGE ON OPERATION TOPIC
+					};
 				}
 				else if (messageArr.length > 2) {
-					for (var key0 in runningProviderTimeFrameObjs) {
-						if (key0 == searchObjTimeFrameQuote) {
-							for (var key in runningProviderTimeFrameObjs[key0]) {
-								if (runningProviderTimeFrameObjs[key0].hasOwnProperty(key)) {
-						  			if (key == messageArr[0]) {
-						  				for(var i=0;i<runningProviderTimeFrameObjs[key0][key].length;i++){
-						  					for (var key1 in runningProviderTimeFrameObjs[key0][key][i]) {
-						  						var tmpObjTimeFrameQuote = runningProviderTimeFrameObjs[key0][key][i];
-						  						if ( Object.keys(tmpObjTimeFrameQuote)[0] == messageArr[1] ) {
-						  							var arrfirstQuotesValues = messageArr[2].split("$");
-						  							for(var k=0;k<arrfirstQuotesValues.length;k++){
-						  								for(var j=0;j<tmpObjTimeFrameQuote[Object.keys(tmpObjTimeFrameQuote)[0]].length;j++){
-						  									var tmpObjSetValuesQuote = tmpObjTimeFrameQuote[Object.keys(tmpObjTimeFrameQuote)[0]][j];
-						  									if (tmpObjSetValuesQuote[Object.keys(tmpObjSetValuesQuote)].length < Object.keys(tmpObjSetValuesQuote)[0].split("v")[1] ){
-						  										tmpObjSetValuesQuote[Object.keys(tmpObjSetValuesQuote)].push(arrfirstQuotesValues[k]);
-						  										tmpObjTimeFrameQuote[Object.keys(tmpObjTimeFrameQuote)[0]][j] = tmpObjSetValuesQuote;
-						  										runningProviderTimeFrameObjs[key0][key][i] = tmpObjTimeFrameQuote;
-						  									}else{
-						  										tmpObjSetValuesQuote[Object.keys(tmpObjSetValuesQuote)].shift();
-		  														tmpObjSetValuesQuote[Object.keys(tmpObjSetValuesQuote)].push(arrfirstQuotesValues[k]);
-		  														tmpObjTimeFrameQuote[Object.keys(tmpObjTimeFrameQuote)[0]][j] = tmpObjSetValuesQuote;
-						  										runningProviderTimeFrameObjs[key0][key][i] = tmpObjTimeFrameQuote;
-						  									}
-						  								}
-						  							}	
-						  						}			
-						  					}
-						  				}
-									}
-								}
-							}
-						}
-					}
+					var result = QuotesModule.importHistoryTimeFrameQuotesObj(searchObjTimeFrameQuote,messageArr);
+					if (result) {}else{console.log("error update timeFrame Obj")};
 				}
 			}
 	}
