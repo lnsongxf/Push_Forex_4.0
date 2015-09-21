538d912b3879c69aaf572ac268aaeec7ec57f4cc
cwicwi <cwicwi2@gmail.com>
2015-09-21 19:09:02 +0200

    corretto output modelli bktfast
    la prima colonna è l'indice delle chiusure, non delle aperture

--- backtest_fast/models/bkt_fast_005_macd.m
+++ backtest_fast/models/bkt_fast_005_macd.m
@@ -79,6 +79,7 @@ classdef bkt_fast_005_macd < handle
                             obj.ClDates(ntrades) = date(indice_I); %controlla
                             %obj = obj.chiudi_per_TP(Pbuy, indice_I, segnoOperazione, devFluct2, wTP, cost, ntrades, date);
                             i = indice_I;
+                            obj.chei(ntrades)=i;
                             indexClose = indexClose + 1;
                             break
                             
@@ -89,6 +90,7 @@ classdef bkt_fast_005_macd < handle
                             obj.ClDates(ntrades) = date(indice_I); %controlla
                             %obj = obj.chiudi_per_SL(Pbuy, indice_I, segnoOperazione, devFluct2, wSL, cost, ntrades, date);
                             i = indice_I;
+                            obj.chei(ntrades)=i;
                             indexClose = indexClose + 1;
                             break
                             
@@ -98,6 +100,7 @@ classdef bkt_fast_005_macd < handle
 %                             obj.closingPrices(ntrades) = Pminute(j);
 %                             obj.ClDates(ntrades) = date(indice_I); %controlla
 %                             i = indice_I;
+%                             obj.chei(ntrades)=i;
 %                             indexClose = indexClose + 1;
 %                             break
 %                             
@@ -161,7 +164,6 @@ classdef bkt_fast_005_macd < handle
             devFluct2 = 1; % lo impongo sempre uguale a 1
             %devFluct2 = std(fluctuationslag((i-(100-M)):i));
             obj.direction(ntrades)= segnoOperazione;
-            obj.chei(ntrades)=i;
             obj.openingPrices(ntrades) = Pbuy;
             obj.OpDates(ntrades) = date(i);
             
