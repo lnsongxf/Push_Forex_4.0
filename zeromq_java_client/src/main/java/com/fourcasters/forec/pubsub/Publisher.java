94154fa1c049f4cc5e5dd16189b6fb590c59551c
Ivan Valeriani <ivaleriani@pushtechnology.com>
2015-10-10 14:40:40 +0100

    Java client for logs and emails

--- zeromq_java_client/src/main/java/com/fourcasters/forec/pubsub/Publisher.java
+++ zeromq_java_client/src/main/java/com/fourcasters/forec/pubsub/Publisher.java
@@ -11,13 +11,15 @@ public class Publisher {
         Context context = ZMQ.context(1);
         Socket publisher = context.socket(ZMQ.PUB);
 
-        publisher.bind("tcp://*:5563");
+        publisher.bind("tcp://*:5562");
+        //publisher.connect("tcp://localhost:5563");
         while (!Thread.currentThread ().isInterrupted ()) {
             // Write two messages, each with an envelope and content
             publisher.sendMore ("A");
             publisher.send ("We don't want to see this");
             publisher.sendMore ("eurusd");
             publisher.send("We would like to see this");
+            Thread.sleep(60000L);
         }
         publisher.close ();
         context.term ();
--- zeromq_java_client/src/main/java/com/fourcasters/forec/pubsub/Subscriber.java
+++ zeromq_java_client/src/main/java/com/fourcasters/forec/pubsub/Subscriber.java
@@ -1,73 +1,83 @@
 package com.fourcasters.forec.pubsub;
 
 import java.nio.charset.Charset;
-import java.util.Arrays;
-import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.zeromq.ZMQ;
 import org.zeromq.ZMQ.Context;
 import org.zeromq.ZMQ.Socket;
 
 public class Subscriber {
 
-	private static final Charset CHARSET = Charset.defaultCharset();
-	private static final String SERVER_ADDRESS = "address";
-	private static final String SERVER_PORT = "port";
-	private static final String TOPICS = "topics";
-	private static String serverAddress;
-	private static Integer port;
-	private static List<String> topics;
-	private static Context context;
-	private static Socket subscriber;
-	private static String connectionString;
-	
-	public static void main(String[] args) {
-		
-		config();
-		
-        connect();
-        
-        subscribe(topics, subscriber);
-        
+	private final static Executor executor = Executors.newSingleThreadExecutor();
+	private static final Logger LOG = LogManager.getLogger(Subscriber.class);
+	private static String password;
+	public static void main (String[] args) {
+		password = args[0];
+        // Prepare our context and subscriber
+        Context context = ZMQ.context(1);
+        Socket subscriber = context.socket(ZMQ.SUB);
+        Socket mailSender = context.socket(ZMQ.SUB);
+        subscriber.connect("tcp://192.168.53.138:50028");
+        mailSender.connect("tcp://192.168.53.138:50027");
+        subscriber.subscribe("LOGS".getBytes());
+        mailSender.subscribe("STATUS".getBytes());
+        LOG.info("Connected");
         while (!Thread.currentThread ().isInterrupted ()) {
-            read(subscriber);
+            // Read envelope with address
+            String address = subscriber.recvStr(Charset.defaultCharset());
+            String contents = subscriber.recvStr (Charset.defaultCharset());
+            LOG.info(address + " = " + contents);
+            address = mailSender.recvStr(ZMQ.NOBLOCK, Charset.defaultCharset());
+            if (address != null && !address.trim().equals("")) {
+            	contents = mailSender.recvStr (Charset.defaultCharset());
+                LOG.info(address + " = " + contents);
+
+            	sendEmail(address, contents);
+            }
         }
-        
-        close();
-	}
-
-	private static void connect() {
-		subscriber.connect(connectionString);
-	}
-
-	private static void close() {
-		subscriber.close ();
+        subscriber.close ();
         context.term ();
+    }
+
+	private static void sendEmail(String address, String contents) {
+		String algoId = parseAlgoId(address);
+		executor.execute(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					Email email = new SimpleEmail();
+					email.setHostName("smtp.gmail.com");
+					email.setSmtpPort(465);
+					email.setAuthenticator(new DefaultAuthenticator("ivan.valeriani", password));
+					email.setSSL(true);
+					email.setFrom("ivan.valeriani@gmail.com");
+					email.setSubject("Automatic trading");
+					email.setMsg(new StringBuffer().append(algoId).append(": ").append(contents).toString());
+					email.addTo("push_it-30@googlegroups.com");
+					email.addTo("phd.alessandro.ricci@gmail.com");
+					email.addTo("cwicwi2@gmail.com");
+					email.addTo("simone.allemanini@gmail.com");
+					email.addTo("ivan.valeriani@gmail.com");
+					email.send();
+				}
+				catch (EmailException e) {
+					LOG.error("Unable to send email.", e);
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
-	private static void config() {
-		serverAddress = System.getProperty(SERVER_ADDRESS, "localhost");
-		port = Integer.getInteger(SERVER_PORT, 5563);
-		topics = Arrays.asList(System.getProperty(TOPICS,"eurusd").split(","));
-		// Prepare our context and subscriber
-        context = ZMQ.context(1);
-        subscriber = context.socket(ZMQ.SUB);
-        connectionString = "tcp://" + serverAddress + ":" + port;
+	private static String parseAlgoId(String address) {
+		String[] tokens = address.split("@");
+		return tokens[tokens.length - 1];
 	}
-
-	private static void subscribe(List<String> topics, Socket subscriber) {
-		for (String topic : topics) {
-			subscriber.subscribe(topic.getBytes());
-		}
-	}
-
-	private static void read(Socket subscriber) {
-		
-        // Read envelope with address
-        String address = subscriber.recvStr (CHARSET);
-        // Read message contents
-        String contents = subscriber.recvStr (CHARSET);
-        System.out.println(address + " : " + contents);
-	}
-	
 }
