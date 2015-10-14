0439ca1fabecf1268628bd46cbc13da8c917c7a0
cwicwi <cwicwi2@gmail.com>
2015-10-14 23:48:02 +0200

    (temporary push) working onlineAlgo002 as onlineAlgoLore2
    need to change names and set the correct timescales for historical
    prices

---
+++ nodeM/MatlabZeroMQ/matlab-zmq/examples/AlgoLore/onlineOpenLore.m
@@ -0,0 +1,25 @@
+function [topicPub,messagePub,startingOperation]=onlineOpenLore(oper,openValue,stopLoss,takeProfit,indexOpen)
+
+%opening
+% in sell con take profit e stop loss settati: op=-1, price=132, lot=1, slippage=05, sl=122, tp=142
+% in buy senza take profit e stop loss settati: op=1, price=132, lot=1, slippage=05op=strcat('op=',num2str(open));
+
+op=strcat('op=',num2str(oper));
+price=strcat('price=',num2str(openValue));
+lots=strcat('lots=',num2str(1));                                           % for the moment we consider always lot=1
+slippage=strcat('slippage=',num2str(1.5));                                 % for the opening slippage=1.5 pips
+magic=strcat('magic=',num2str(720));                                       % magic number, algo id.
+sl=strcat('sl=',num2str(stopLoss));                                        % not madatory
+tp=strcat('tp=',num2str(takeProfit));                                      % not mandatory
+topicPub='OPERATIONS@ACTIVTRADES@EURUSD@720';
+messagePub=strcat(price,',',lots,',',slippage,',',sl,',',tp,',',magic,',',op);
+
+indexOpen = indexOpen + 1;
+display(['indexOpen =' num2str(indexOpen)]);
+display(['direction =' num2str(oper)]);
+
+display('Matalb requests to open a new operation');
+
+startingOperation = oper;
+
+end
