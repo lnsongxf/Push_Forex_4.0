4bea687e14f800c55e4c22f6fa3755738e4ea879
alessandro ricci <phd.alessandro.ricci@gmail.com>
2015-12-29 15:46:35 +0100

    including progressiveTPandSL.m

---
+++ 02_closingManager/progressiveTPandSL.m
@@ -0,0 +1,26 @@
+function [newTakeP,newStopL] = progressiveTPandSL (LastClosePrice,OpenPrice,TakeProfitPrice,StopLossPrice,direction,TakeP,StopL,variables)
+
+tresholdSL = variables(1);
+newLimitTP = variables(2);
+newLimitSL = variables(3);
+
+newTakeP = TakeP;
+newStopL = StopL;
+
+% If the current price is more than SL above the opening prcice, re-set
+if abs( (LastClosePrice - StopLossPrice) ) > abs(StopL) * tresholdSL
+    
+    distance = floor(abs(LastClosePrice - StopLossPrice)/2);
+    
+    newStopL =  - direction * ( (LastClosePrice - OpenPrice) - direction * distance );
+    
+end
+
+% If the current price is above half TakeP, re-set the StopL and TakeP
+if ( (LastClosePrice - TakeProfitPrice) * direction ) >= 0
+    
+    newTakeP = TakeP + abs(LastClosePrice - TakeProfitPrice) + newLimitTP;
+
+    newStopL = - TakeP + newLimitSL;
+
+end
