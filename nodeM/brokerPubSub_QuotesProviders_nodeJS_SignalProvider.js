8b34d301f1a09500bb01fc61409b392f53bf2259
Simone Allemanini <Simone.A@brandtix.com>
2016-03-15 16:27:21 +0000

    embedded node server

--- nodeM/brokerPubSub_QuotesProviders_nodeJS_SignalProvider.js
+++ nodeM/brokerPubSub_QuotesProviders_nodeJS_SignalProvider.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var zmq = require('zmq'); 
 var schedule = require('node-schedule');
+var bunyan = require('bunyan');
+var BunyanSlack = require('bunyan-slack');
 
 /*
 "fatal" (60): The service/app is going to stop or become unusable now. An operator should definitely look into this soon.
@@ -579,6 +581,104 @@ sockSubFromSignalProvider.on('message', function() {
 	}
 
 });
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////        LOG SERVER         /////////////////////////////
+
+setTimeout(function(){
+
+	var loggerSockSub = zmq.socket('sub');
+	loggerSockSub.connect('tcp://localhost:50028');
+
+	var logStore = bunyan.createLogger({
+		name: '4casterLogApp',
+		streams: [
+	    {
+	    	level: 'trace',
+	      	stream: process.stdout         // log INFO and above to stdout
+	    },
+	    {
+	    	level: 'error',
+	    	path: '../../NodeLogs/4casterLogApp-error.log',  // log INFO and above to a file
+	    	period: '1d',   // daily rotation
+	    	count: 5        // keep 3 back copies
+	    },
+	    {
+	    	level: 'info',
+	    	path: '../../NodeLogs/4casterLogApp-info.log',  // log INFO and above to a file
+	    	period: '1d',   // daily rotation
+	    	count: 5       // keep 3 back copies
+	    },
+	    {
+	      	level: 'fatal',
+	      	path: '../../NodeLogs/4casterLogApp-fatal.log',  // log INFO and above to a file
+	      	period: '1d',   // daily rotation
+	      	count: 5        // keep 3 back copies
+	    }
+	  ]
+	});
+
+
+	var logMessaging = bunyan.createLogger({
+	  name: '4casterLogApp',
+	  streams: [
+	    {
+	      level: 'error',
+	      stream: new BunyanSlack({
+	        webhook_url: "https://hooks.slack.com/services/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",
+	        channel: "#logs-node-beta",
+	        username: "admin",
+	        customFormatter: function(record, levelName){
+	            return {text: "[" + levelName + "] " + record.msg }
+	        }
+	      })
+	    },
+	    {
+	      level: 'info',
+	      stream: new BunyanSlack({
+	        webhook_url: "https://hooks.slack.com/services/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",
+	        channel: "#logs-node-beta",
+	        username: "admin",
+	        customFormatter: function(record, levelName){
+	            return {text: "[" + levelName + "] " + record.msg }
+	        }
+	      })
+	    },
+	    {
+	      level: 'fatal',
+	      stream: new BunyanSlack({
+	        webhook_url: "https://hooks.slack.com/services/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",
+	        channel: "#logs-node-beta",
+	        username: "admin",
+	        customFormatter: function(record, levelName){
+	            return {text: "[" + levelName + "] " + record.msg }
+	        }
+	      })
+	    }
+	  ]
+	});
+
+	loggerSockSub.subscribe('LOGS');
+	loggerSockSub.on('message', function(topic, message) {
+
+		if (topic == "LOGS@INFO") {
+			logStore.info(message.toString());
+	    	logMessaging.info(message.toString());
+		}else if (topic == "LOGS@FATAL") {
+			logStore.fatal(message.toString());
+	    	logMessaging.fatal(message.toString());
+		}else if( topic == "LOGS@ERROR" ){
+			logStore.error(message.toString());
+	    	logMessaging.error(message.toString());
+		}else if (topic == "LOGS@TRACE") {
+			logStore.trace(message.toString());
+		};
+	});
+
+},60000);
 
 
 
