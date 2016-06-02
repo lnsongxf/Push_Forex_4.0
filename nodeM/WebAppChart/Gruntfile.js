9012198350b0b832b5ae4550619235f93520e8ed
Simone Allemanini <Simone.A@brandtix.com>
2016-06-02 23:43:47 +0100

    deleted old files

--- nodeM/WebAppChart/Gruntfile.js
+++ nodeM/WebAppChart/Gruntfile.js
@@ -63,7 +63,8 @@ module.exports = function (grunt) {
       options: {
         port: 9005,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        // Change this to 'localhost' to access the server from inside.
+        hostname: '0.0.0.0',
         livereload: 35729
       },
       livereload: {
