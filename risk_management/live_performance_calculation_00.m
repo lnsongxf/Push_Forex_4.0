8ee880e7b586488b2a0552fe1fb0132b7cbc4a64
ettoremaiorana <ettoremaiorana@tiscali.it>
2016-08-01 23:37:17 +0100

    input passed through command line may be interpreted as string rather than numers

--- risk_management/live_performance_calculation_00.m
+++ risk_management/live_performance_calculation_00.m
@@ -3,6 +3,12 @@
 % - timeFrame
 % - cross
 function live_performance_calculation_00(algoId, timeFrame, cross)
+    if ischar(algoId)
+        algoId = str2double(algoId);
+    end
+    if ischar(timeFrame)
+        timeFrame = str2double(timeFrame);
+    end
     m = fromWebPageToMatrix(algoId, timeFrame, 'Trades.csv');
     perf = Performance_07;
     perf.calcSinglePerformance(num2str(algoId), 'demo', '', cross, timeFrame, 0, 10000, 1, m, 0)
