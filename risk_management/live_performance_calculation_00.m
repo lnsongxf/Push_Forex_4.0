810daf0983de8f15e2c39aed3a41a568104da06e
Ricci <alericci@win.desy.de>
2016-08-01 18:42:21 +0200

    new exe created from live_performance_calculation

--- risk_management/live_performance_calculation_00.m
+++ risk_management/live_performance_calculation_00.m
@@ -2,7 +2,7 @@
 % - algoId
 % - timeFrame
 % - cross
-function live_performance_calculation(algoId, timeFrame, cross)
+function live_performance_calculation_00(algoId, timeFrame, cross)
     m = fromWebPageToMatrix(algoId, timeFrame, 'Trades.csv');
     perf = Performance_07;
     perf.calcSinglePerformance(num2str(algoId), 'demo', '', cross, timeFrame, 0, 10000, 1, m, 0)
