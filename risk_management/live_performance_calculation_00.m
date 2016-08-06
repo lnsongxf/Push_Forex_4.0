7fbc2f2cc4e1f2590baff7569954b8edba24c840
ettoremaiorana <ettoremaiorana@tiscali.it>
2016-08-06 13:25:27 +0100

    Wrap performance calculation around live call

--- risk_management/live_performance_calculation_00.m
+++ risk_management/live_performance_calculation_00.m
@@ -9,10 +9,7 @@ function live_performance_calculation_00(algoId, timeFrame, cross)
     if ischar(timeFrame)
         timeFrame = str2double(timeFrame);
     end
-    m = fromWebPageToMatrix(algoId, timeFrame, 'Trades.csv');
-    perf = Performance_07;
-    perf.calcSinglePerformance(num2str(algoId), 'demo', '', cross, timeFrame, 0, 10000, 1, m, 0)
-    perf.serialize(strcat(num2str(algoId), '_performance.csv'));
+    performance_calculation(algoId, timeFrame, cross);
     clear all;
     exit;
 end
